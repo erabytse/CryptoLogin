d08ee3b1fd1b4e4f4670b04e211f173ac3d9510e
TakoUlzO <f.bfalik@gmail.com>
2026-06-23 13:01:53 +0200

    fix: Update CORS middleware to change allowed origin from 127.0.0.1:8080 to 127.0.0.1:4000

--- cryptologin/main.py
+++ cryptologin/main.py
@@ -83,7 +83,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://127.0.0.1:8080",           
+        "http://127.0.0.1:4000",           
         "http://localhost:8080",           
         "http://localhost:3000",           
         "http://localhost:8000",
